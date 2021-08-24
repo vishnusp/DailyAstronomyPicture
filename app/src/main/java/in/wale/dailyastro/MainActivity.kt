251c27b34f5050f7bca9984052bf08067f60b5a2
Vishnu S Prasad <v.vishnusp@gmail.com>
2021-08-24 23:45:55 +0530

    Initial Commit: Base folder structure for the project

---
+++ app/src/main/java/in/wale/dailyastro/MainActivity.kt
@@ -0,0 +1,11 @@
+package `in`.wale.dailyastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
